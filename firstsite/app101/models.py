25507e12c91e219aecf9cb95c5f8b0207d4fdcbd
1666sApple <ananno.034@gmail.com>
2024-06-23 04:34:35 +0600

    Modified Site Design

--- firstsite/app101/models.py
+++ firstsite/app101/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Item(models.Model):
-    
+
     def __str__(self):
         return self.name
-    
+
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.CharField(max_length=500, default="https://nolabelatthetable.com/wp-content/uploads/2018/01/IMAGE-COMING-SOON-600x600.png")
--- firstsite/firstsite/urls.py
+++ firstsite/firstsite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app101 import urls
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +25,6 @@ urlpatterns = [
     path('app101/', include('app101.urls')),
     path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
